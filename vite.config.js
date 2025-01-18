b0cf3d0ccc04525499fe78fb766bf36a839285b4
AbdulwahidHusein <abdulwahidhussen10@gmail.com>
2025-01-18 15:17:01 -0500

    groq-sdk alias import fix try

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['groq-sdk']
+  resolve: {
+    alias: {
+      'groq-sdk': '/node_modules/groq-sdk/dist/index.js'
     }
   }
 })
